1eeeb20f857f38bc1404a5b7936f86400c816edd
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-02 15:29:13 +0530

    create Company Controller and Routes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from "./utils/db.js";
+
+//routes
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js"
 
 dotenv.config({});
 const app = express();
@@ -22,6 +25,7 @@ const PORT = process.env.PORT || 8081;
 
 //api
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
 
 
 app.listen(PORT, ()=>{
--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -5,6 +5,7 @@ const companySchema = new mongoose.Schema(
     name: {
       type: String,
       require: true,
+      unique: true,
     },
     description: {
       type: String,
