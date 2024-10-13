594a971e89d197d153392d9d5ccad30d187fe075
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-14 01:36:49 +0530

    changes in constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,16 +1,17 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "https://job-portal-ab25.onrender.com/api/v1";
 
-export const USER_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/user";
+export const USER_API_END_POINT = BASE_URL + "/user";
 // export const USER_API_END_POINT = BASE_URL + "/user";
 
 
-export const JOB_API_END_POINT = "https://job-portal-ab25.onrender.com/job";
+export const JOB_API_END_POINT = BASE_URL + "/job";
 // export const JOB_API_END_POINT = BASE_URL + "/job";
 
 
-export const APPLICATION_API_END_POINT = "https://job-portal-ab25.onrender.com/application";
+export const APPLICATION_API_END_POINT =  BASE_URL + "/application";
 // export const APPLICATION_API_END_POINT = BASE_URL + "/application";
 
 
-export const COMPANY_API_END_POINT = "https://job-portal-ab25.onrender.com/company";
+export const COMPANY_API_END_POINT = BASE_URL + "/company";
 // export const COMPANY_API_END_POINT = BASE_URL + "/company";
