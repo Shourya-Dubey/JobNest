47e94175b3fd15cf5f7309658311c6f0d574e3f9
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-14 00:53:03 +0530

    added backend Render Url to constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,16 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
+export const USER_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/user";
+// export const USER_API_END_POINT = BASE_URL + "/user";
 
-export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
 
-export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
+export const JOB_API_END_POINT = "https://job-portal-ab25.onrender.com/job";
+// export const JOB_API_END_POINT = BASE_URL + "/job";
+
+
+export const APPLICATION_API_END_POINT = "https://job-portal-ab25.onrender.com/application";
+// export const APPLICATION_API_END_POINT = BASE_URL + "/application";
+
+
+export const COMPANY_API_END_POINT = "https://job-portal-ab25.onrender.com/company";
+// export const COMPANY_API_END_POINT = BASE_URL + "/company";
