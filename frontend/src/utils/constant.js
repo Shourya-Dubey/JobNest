af07f24bdae6d2b178b5aaf237efe067d44e59cb
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-14 11:04:48 +0530

    change base_url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,18 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+const BASE_URL = "https://job-portal-ab25.onrender.com";
 
-export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
 
-export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
 
-export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
+// export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+export const USER_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/user";
+
+
+// export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
+export const JOB_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/job";
+
+
+// export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
+export const APPLICATION_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/application";
+
+
+// export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
+export const COMPANY_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/company";
