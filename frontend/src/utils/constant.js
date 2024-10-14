037751346d215db274661d814d426a6a7c60595f
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-14 10:47:24 +0530

    change something

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,17 +1,7 @@
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL = "https://job-portal-ab25.onrender.com/api/v1";
+export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
 
-export const USER_API_END_POINT = BASE_URL + "/user";
-// export const USER_API_END_POINT = BASE_URL + "/user";
+export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
 
+export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
 
-export const JOB_API_END_POINT = BASE_URL + "/job";
-// export const JOB_API_END_POINT = BASE_URL + "/job";
-
-
-export const APPLICATION_API_END_POINT =  BASE_URL + "/application";
-// export const APPLICATION_API_END_POINT = BASE_URL + "/application";
-
-
-export const COMPANY_API_END_POINT = BASE_URL + "/company";
-// export const COMPANY_API_END_POINT = BASE_URL + "/company";
+export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
