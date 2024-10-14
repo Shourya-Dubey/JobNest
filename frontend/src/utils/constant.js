2e1c23558fd6c388673222ad1822d4d4d5c1c597
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-10-14 11:13:15 +0530

    Adding BASE_URL

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,18 +1,18 @@
-const BASE_URL = "https://job-portal-ab25.onrender.com";
+const URL = process.env.BASE_URL
 
 
 
 // export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const USER_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/user";
+export const USER_API_END_POINT = URL + "/api/v1/user";
 
 
 // export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
-export const JOB_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/job";
+export const JOB_API_END_POINT = URL + "/api/v1/job";
 
 
 // export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
-export const APPLICATION_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/application";
+export const APPLICATION_API_END_POINT = URL + "/api/v1/application";
 
 
 // export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
-export const COMPANY_API_END_POINT = "https://job-portal-ab25.onrender.com/api/v1/company";
+export const COMPANY_API_END_POINT = URL + "/api/v1/company";
